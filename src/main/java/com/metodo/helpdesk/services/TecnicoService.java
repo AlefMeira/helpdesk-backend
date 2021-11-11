39af01e47c77e02bbcc9169ff5f5d6a52d1c0106
Alefchaves <alef.chaves01@gmail.com>
2021-11-11 15:57:11 -0300

    endpoint  findById para Tecnico

---
+++ src/main/java/com/metodo/helpdesk/services/TecnicoService.java
@@ -0,0 +1,27 @@
+package com.metodo.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.metodo.helpdesk.domain.Tecnico;
+import com.metodo.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = tecnicoRepository.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+				
+	
+	
+	
+}
