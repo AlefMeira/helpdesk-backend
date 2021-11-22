a03ca12b9f4635271e3f26cbbec95c11d1788141
Alefchaves <alef.chaves01@gmail.com>
2021-11-22 08:38:23 -0300

    findAll para Chamado

--- src/main/java/com/metodo/helpdesk/services/ChamadoService.java
+++ src/main/java/com/metodo/helpdesk/services/ChamadoService.java
@@ -1,33 +1,29 @@
 package com.metodo.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.metodo.helpdesk.domain.Chamado;
+import com.metodo.helpdesk.domain.dtos.ChamadoDTO;
 import com.metodo.helpdesk.repositories.ChamadoRepository;
 import com.metodo.helpdesk.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ChamadoService {
-	
+
 	@Autowired
 	private ChamadoRepository chamadoRepository;
-	
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = chamadoRepository.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
 	}
 	
-	
-
-	
-	
-	
-	
-	
-	
-	
+	public List<Chamado> findAll() {
+		return chamadoRepository.findAll();
+	}
 
 }
